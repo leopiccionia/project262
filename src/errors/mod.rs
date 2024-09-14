56ffa01b00f3455b2915184baad924f3d0fabe61
leopiccionia <leopiccionia@gmail.com>
2024-09-14 00:47:34 -0300

    Improve documentation of `CoreResult`

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -20,4 +20,6 @@ pub enum CoreError {
 }
 
 /// A [Result] wrapping either a `T` or a [CoreError].
+///
+/// It can be converted into a [`Completion`](crate::completions::Completion) via the [`From`] trait.
 pub type CoreResult<T> = Result<T, CoreError>;
